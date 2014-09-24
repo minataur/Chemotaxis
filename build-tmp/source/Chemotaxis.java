fabe634e2d0b33cbf2c6db8254c6d17250a27b82
minataur <minac.98sf@gmail.com>
2014-09-24 11:26:36 -0700

    chemotaxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  Bacteria[] suzy; 
- void setup()   
+ public void setup()   
  {     
  	size(300,300);//initialize bacteria variables here
  	fill(0);
@@ -12,7 +28,7 @@
  	} 
  }   
  
- void draw() {
+ public void draw() {
  	for (int x=0; x < 50; x++){
  		suzy[x].show();
  		suzy[x].walk(); 
@@ -24,7 +40,7 @@
   
  } 
 
- void mousePressed() {
+ public void mousePressed() {
 
  } 
  
@@ -39,14 +55,14 @@
  		move = 0; //0= no bias; 1= right, 2 = left, 3= up, 4=down
  	}
 
- 	void show() {
+ 	public void show() {
  		
  		noStroke();
  		fill(myColor, 25);
  		ellipse(myX,myY,10,10);
  	} 
 
- 	void walk() {
+ 	public void walk() {
  		myY+=(int)(Math.random()*7)-direction;
  		myX+=(int)(Math.random()*7)-direction;
  		if (myY>0 && myY<300) {
@@ -67,3 +83,12 @@
  	} 
  	 
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
